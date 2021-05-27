7df45f7cf591d2ab8afd8cc10f0951adb7c582a8
breid <breid@activecampaign.com>
2021-05-27 10:37:03 -0700

    finish v1 of plugin

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# gcal-screenshotter
+Simple chrome plugin for anonymizing events on google calendar. That way you can take and share a screenshot and easily provide your availability to someone.
--- background.js
+++ background.js
@@ -1,26 +1,25 @@
 chrome.runtime.onInstalled.addListener(function() {
   chrome.tabs.onActivated.addListener(async info => {
     const tab = await chrome.tabs.get(info.tabId);
-
     const isGoogleCalendar = tab.url.startsWith('https://calendar.google.com/');
 
     if (isGoogleCalendar) {
       chrome.action.setIcon({
         path: {
-          "16": "/images/get_started16.png",
-          "32": "/images/get_started32.png",
-          "48": "/images/get_started48.png",
-          "128": "/images/get_started128.png"
+          "16": "/images/icon_16.png",
+          "32": "/images/icon_32.png",
+          "48": "/images/icon_48.png",
+          "128": "/images/icon_128.png"
         }
       });
       chrome.action.enable(tab.tabId);
     } else {
       chrome.action.setIcon({
         path: {
-          "16": "/images/get_started16_disabled.png",
-          "32": "/images/get_started32_disabled.png",
-          "48": "/images/get_started48_disabled.png",
-          "128": "/images/get_started128_disabled.png"
+          "16": "/images/icon_16_disabled.png",
+          "32": "/images/icon_32_disabled.png",
+          "48": "/images/icon_48_disabled.png",
+          "128": "/images/icon_128_disabled.png"
         }
       });
       chrome.action.disable(tab.tabId);
